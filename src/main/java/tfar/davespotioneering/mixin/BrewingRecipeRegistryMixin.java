587849d55ab2d7815b27f2607b4a55b5e8a781b8
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2022-10-16 18:35:34 -0400

    fix #3

--- src/main/java/tfar/davespotioneering/mixin/BrewingRecipeRegistryMixin.java
+++ src/main/java/tfar/davespotioneering/mixin/BrewingRecipeRegistryMixin.java
@@ -9,12 +9,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(BrewingRecipeRegistry.class)
 public class BrewingRecipeRegistryMixin {
 
-    //makes conditional in BrewingRecipeRegistry always return 1 so that shift clicking and item transfer work better
-    @Redirect(method = "isValidInput",at = @At(value = "INVOKE",target = "Lnet/minecraft/world/item/ItemStack;getCount()I"))
-    private static int always1(ItemStack stack) {
-        return 1;
-    }
-
     //makes conditional in BrewingRecipeRegistry always return 1 so brewing check works for >1 potion
     @Redirect(method = "getOutput",at = @At(value = "INVOKE",target = "Lnet/minecraft/world/item/ItemStack;getCount()I"))
     private static int alwaysOne(ItemStack stack) {
