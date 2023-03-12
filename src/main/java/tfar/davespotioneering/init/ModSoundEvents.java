ff3e289953e16b0f80f68eb2f738829cb4b50d84
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-03-12 00:31:59 -0500

    add sounds

--- src/main/java/tfar/davespotioneering/init/ModSoundEvents.java
+++ src/main/java/tfar/davespotioneering/init/ModSoundEvents.java
@@ -13,6 +13,10 @@ public class ModSoundEvents {
     public static final SoundEvent GAUNTLET_SCROLL = createSound("gauntlet_scroll");
     public static final SoundEvent GAUNTLET_TURNING_ON = createSound("gauntlet_turning_on");
     public static final SoundEvent GAUNTLET_TURNING_OFF = createSound("gauntlet_turning_off");
+    public static final SoundEvent BUBBLING_WATER_CAULDRON = createSound("bubbling_water_cauldron");
+    public static final SoundEvent UMBRELLA_OPEN = createSound("umbrella_open");
+    public static final SoundEvent UMBRELLA_CLOSE = createSound("umbrella_close");
+
     public static final SoundEvent GAUNTLET_EQUIP = createSound("gauntlet_equip");
 
 
--- src/main/java/tfar/davespotioneering/item/UmbrellaItem.java
+++ src/main/java/tfar/davespotioneering/item/UmbrellaItem.java
@@ -4,6 +4,11 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShieldItem;
@@ -12,6 +17,7 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.client.IItemRenderProperties;
 import net.minecraftforge.common.util.NonNullLazy;
 import tfar.davespotioneering.init.ModItems;
+import tfar.davespotioneering.init.ModSoundEvents;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -37,6 +43,17 @@ public class UmbrellaItem extends ShieldItem {
         tooltip.add(new TextComponent(style));
 
         tooltip.add(new TranslatableComponent(getDescriptionId()+".desc"));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        pLevel.playLocalSound(pPlayer.getX(),pPlayer.getY(),pPlayer.getZ(), ModSoundEvents.UMBRELLA_OPEN, SoundSource.BLOCKS,.5f,1,false);
+        return super.use(pLevel, pPlayer, pHand);
+    }
+
+    @Override
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity, int pTimeCharged) {
+        pLevel.playLocalSound(pLivingEntity.getX(),pLivingEntity.getY(),pLivingEntity.getZ(), ModSoundEvents.UMBRELLA_CLOSE, SoundSource.BLOCKS,.5f,1,false);
     }
 
     @Override
