6f86a50dd58c47f7751ab97d87da5d1c5143c73d
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-03-12 00:49:46 -0500

    Update LayeredReinforcedCauldronBlock.java

--- src/main/java/tfar/davespotioneering/block/LayeredReinforcedCauldronBlock.java
+++ src/main/java/tfar/davespotioneering/block/LayeredReinforcedCauldronBlock.java
@@ -30,7 +30,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.levelgen.RandomSource;
 import net.minecraft.world.phys.AABB;
 import tfar.davespotioneering.blockentity.ReinforcedCauldronBlockEntity;
 import tfar.davespotioneering.init.ModBlocks;
