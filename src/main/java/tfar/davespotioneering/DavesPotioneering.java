e41da3319347e2addc0cbb221871bd3e214fa656
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-09-04 14:30:49 -0400

    Update DavesPotioneering.java

--- src/main/java/tfar/davespotioneering/DavesPotioneering.java
+++ src/main/java/tfar/davespotioneering/DavesPotioneering.java
@@ -16,6 +16,7 @@ import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
@@ -92,22 +93,22 @@ public class DavesPotioneering {
     }
 
     private void register(RegisterEvent e) {
-        superRegister(e, ModBlocks.class,Registry.BLOCK_REGISTRY,Block.class);
-        superRegister(e, ModItems.class,Registry.ITEM_REGISTRY,Item.class);
-        superRegister(e, ModBlockEntityTypes.class,Registry.BLOCK_ENTITY_TYPE_REGISTRY,BlockEntityType.class);
-        superRegister(e, ModMenuTypes.class,Registry.MENU_REGISTRY, MenuType.class);
-        superRegister(e, ModEffects.class,Registry.MOB_EFFECT_REGISTRY, MobEffect.class);
-        superRegister(e, ModParticleTypes.class,Registry.PARTICLE_TYPE_REGISTRY, ParticleType.class);
-        superRegister(e, ModPotions.class,Registry.POTION_REGISTRY,Potion.class);
-        superRegister(e, ModSoundEvents.class,Registry.SOUND_EVENT_REGISTRY,SoundEvent.class);
+        superRegister(e, ModBlocks.class, Registry.BLOCK_REGISTRY, Block.class);
+        superRegister(e, ModItems.class, Registry.ITEM_REGISTRY, Item.class);
+        superRegister(e, ModBlockEntityTypes.class, Registry.BLOCK_ENTITY_TYPE_REGISTRY, BlockEntityType.class);
+        superRegister(e, ModMenuTypes.class, Registry.MENU_REGISTRY, MenuType.class);
+        superRegister(e, ModEffects.class, Registry.MOB_EFFECT_REGISTRY, MobEffect.class);
+        superRegister(e, ModParticleTypes.class, Registry.PARTICLE_TYPE_REGISTRY, ParticleType.class);
+        superRegister(e, ModPotions.class, Registry.POTION_REGISTRY, Potion.class);
+        superRegister(e, ModSoundEvents.class, Registry.SOUND_EVENT_REGISTRY, SoundEvent.class);
     }
 
-    public static <T> void superRegister(RegisterEvent e, Class<?> clazz, ResourceKey<? extends  Registry<T>> resourceKey, Class<?> filter) {
+    public static <T> void superRegister(RegisterEvent e, Class<?> clazz, ResourceKey<? extends Registry<T>> resourceKey, Class<?> filter) {
         for (Field field : clazz.getFields()) {
             try {
                 Object o = field.get(null);
                 if (filter.isInstance(o)) {
-                    e.register(resourceKey,new ResourceLocation(MODID,field.getName().toLowerCase(Locale.ROOT)),() -> (T)o);
+                    e.register(resourceKey, new ResourceLocation(MODID, field.getName().toLowerCase(Locale.ROOT)), () -> (T) o);
                 }
             } catch (IllegalAccessException illegalAccessException) {
                 illegalAccessException.printStackTrace();
@@ -116,30 +117,30 @@ public class DavesPotioneering {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        Util.setStackSize(Items.POTION,16);
-        Util.setStackSize(Items.SPLASH_POTION,4);
-        Util.setStackSize(Items.LINGERING_POTION,4);
+        Util.setStackSize(Items.POTION, 16);
+        Util.setStackSize(Items.SPLASH_POTION, 4);
+        Util.setStackSize(Items.LINGERING_POTION, 4);
 
         Events.register();
 
         ItemStack milkPot = new ItemStack(Items.POTION);
-        PotionUtils.setPotion(milkPot,ModPotions.MILK);
+        PotionUtils.setPotion(milkPot, ModPotions.MILK);
 
         ItemStack splashMilkPot = new ItemStack(Items.SPLASH_POTION);
-        PotionUtils.setPotion(splashMilkPot,ModPotions.MILK);
+        PotionUtils.setPotion(splashMilkPot, ModPotions.MILK);
 
         ItemStack lingerMilkPot = new ItemStack(Items.LINGERING_POTION);
-        PotionUtils.setPotion(lingerMilkPot,ModPotions.MILK);
+        PotionUtils.setPotion(lingerMilkPot, ModPotions.MILK);
 
-        BrewingRecipeRegistry.addRecipe(PotionIngredient.create(milkPot),Ingredient.of(Items.GUNPOWDER),splashMilkPot);
+        BrewingRecipeRegistry.addRecipe(PotionIngredient.create(milkPot), Ingredient.of(Items.GUNPOWDER), splashMilkPot);
 
-        BrewingRecipeRegistry.addRecipe(PotionIngredient.create(milkPot),Ingredient.of(Items.DRAGON_BREATH),lingerMilkPot);
+        BrewingRecipeRegistry.addRecipe(PotionIngredient.create(milkPot), Ingredient.of(Items.DRAGON_BREATH), lingerMilkPot);
 
-        strongRecipe(Potions.INVISIBILITY,ModPotions.STRONG_INVISIBILITY);
+        strongRecipe(Potions.INVISIBILITY, ModPotions.STRONG_INVISIBILITY);
 
-        Set<Block> newSet = new HashSet<>(((BlockEntityTypeAcces)BlockEntityType.LECTERN).getValidBlocks());
+        Set<Block> newSet = new HashSet<>(((BlockEntityTypeAcces) BlockEntityType.LECTERN).getValidBlocks());
         newSet.add(ModBlocks.MAGIC_LECTERN);
-        ((BlockEntityTypeAcces)BlockEntityType.LECTERN).setValidBlocks(newSet);
+        ((BlockEntityTypeAcces) BlockEntityType.LECTERN).setValidBlocks(newSet);
 
         PacketHandler.registerMessages();
 
@@ -148,21 +149,21 @@ public class DavesPotioneering {
     }
 
     private void stackAdj(ServerStartingEvent e) {
-            Util.setStackSize(Items.POTION,ModConfig.Server.potion_stack_size.get());
-            Util.setStackSize(Items.SPLASH_POTION,ModConfig.Server.splash_potion_stack_size.get());
-            Util.setStackSize(Items.LINGERING_POTION,ModConfig.Server.lingering_potion_stack_size.get());
+        Util.setStackSize(Items.POTION, ModConfig.Server.potion_stack_size.get());
+        Util.setStackSize(Items.SPLASH_POTION, ModConfig.Server.splash_potion_stack_size.get());
+        Util.setStackSize(Items.LINGERING_POTION, ModConfig.Server.lingering_potion_stack_size.get());
     }
 
-    private void stackAdj1(NetworkEvent.ClientCustomPayloadLoginEvent e) {
-        Util.setStackSize(Items.POTION,ModConfig.Server.potion_stack_size.get());
-        Util.setStackSize(Items.SPLASH_POTION,ModConfig.Server.splash_potion_stack_size.get());
-        Util.setStackSize(Items.LINGERING_POTION,ModConfig.Server.lingering_potion_stack_size.get());
+    private void stackAdj1(ClientPlayerNetworkEvent.LoggingIn e) {
+        Util.setStackSize(Items.POTION, ModConfig.Server.potion_stack_size.get());
+        Util.setStackSize(Items.SPLASH_POTION, ModConfig.Server.splash_potion_stack_size.get());
+        Util.setStackSize(Items.LINGERING_POTION, ModConfig.Server.lingering_potion_stack_size.get());
     }
 
-    protected static void strongRecipe(Potion potion,Potion strong) {
+    protected static void strongRecipe(Potion potion, Potion strong) {
         BrewingRecipeRegistry.addRecipe(PotionIngredient.create(
-                        PotionUtils.setPotion(new ItemStack(Items.POTION),potion)),
-                        Ingredient.of(Items.GLOWSTONE_DUST),
-                        PotionUtils.setPotion(new ItemStack(Items.POTION), strong));
+                        PotionUtils.setPotion(new ItemStack(Items.POTION), potion)),
+                Ingredient.of(Items.GLOWSTONE_DUST),
+                PotionUtils.setPotion(new ItemStack(Items.POTION), strong));
     }
 }
