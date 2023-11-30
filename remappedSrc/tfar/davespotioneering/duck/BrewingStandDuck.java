ff5341c1fff6369f4401e00300058aa137ccf57f
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-11-29 22:07:01 -0500

    why is that there?

--- remappedSrc/tfar/davespotioneering/duck/BrewingStandDuck.java
+++
@@ -1,9 +0,0 @@
-package tfar.davespotioneering.duck;
-
-import net.minecraft.world.entity.player.Player;
-
-public interface BrewingStandDuck {
-
-    void addXp(double xp);
-    void dump(Player player);
-}
